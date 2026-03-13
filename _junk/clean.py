c4a79f00836e0220f56ce074b07ef8baced04813
SiliQon <74933065+SiliqonDev@users.noreply.github.com>
2026-03-14 03:37:52 +0530

    stop tracking clean.py

--- _junk/clean.py
+++
@@ -1,9 +0,0 @@
-good = []
-with open("ospd.txt", 'r') as f:
-    for l in f:
-        l = l.strip()
-        if len(l) == 5:
-            good.append(l+"\n")
-
-with open("clean-ospd.txt", 'w') as f:
-    f.writelines(good)
